bd6311b5954a5b4e65c0917d59b78f6fe83319b3
Jim Whatley <jewhatl@gmail.com>
2024-09-12 14:06:21 -0700

    writing dice game

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿Random dice = new();
 
-int firstValue = 500;
-int secondValue = 600;
-int largerValue = Math.Max(firstValue, secondValue);
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
 
-Console.WriteLine(largerValue);
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if (total > 15)
+{
+    Console.WriteLine("You win!");
+}
+
+if (total < 15)
+{
+    Console.WriteLine("Sorry, you lose.");
+}
