3636e42e1847429fcfcba9b5952f9b0ebcd50ed3
Jim Whatley <jewhatl@gmail.com>
2024-09-13 05:26:40 -0700

    Saving for later

--- Program.cs
+++ Program.cs
@@ -1,50 +1,30 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Random dice = new();
+﻿Random random = new();
 
-        int roll1 = dice.Next(1, 7);
-        int roll2 = dice.Next(1, 7);
-        int roll3 = dice.Next(1, 7);
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-//        roll1 = 1;
-//        roll2 = 2;
-//        roll1 = 1;
+/*if <= 10
+   ""
+else if <= 5
+   ""
+else if == 1
+   ""
+else
+   ""  */
 
-        int total = roll1 + roll2 + roll3;
+   Console.WriteLine(daysUntilExpiration);
 
-        Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-        if (roll1 == roll2 || roll2 == roll3 | roll1 == roll3)
-        {
-            if (roll1 == roll2 && roll2 == roll3)
-            {
-                Console.WriteLine("You rolled triples!");
-                total += 6;
-            }
-            else
-            {
-                Console.WriteLine("You rolled doubles! +2 bonus to total");
-                total += 2;
-            }
-
-        }
-
-        Console.WriteLine($"Your total including the bonus: {total}");
-
-
-        if (total >= 16)
-        {
-            Console.WriteLine("You win a new car!");
-        }
-        else if(total >= 10)
-        {
-            Console.WriteLine("You win a new laptap!");
-        }else if(total == 7){
-            Console.WriteLine("You win a trip for two!");
+if (daysUntilExpiration <= 10) {
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+    } else if(daysUntilExpiration <= 5){
+        Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days until expiration.");
+        Console.WriteLine("Renew now and save 10%");
+        discountPercentage = 10;
+        } else if(daysUntilExpiration == 1){
+            Console.WriteLine("Your subscription expires within a day!");
+            Console.WriteLine("Renew now and save 20%");
+            discountPercentage = 20;
         }else{
-            Console.WriteLine("You win a kitten!!");
+            Console.WriteLine("Your subscription has expired");
         }
-    }
-}
+    
